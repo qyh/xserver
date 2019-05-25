5202a9b7389a5a04ab4eaba8406af0a236ea30b2
qyh <704344856@qq.com>
2019-05-25 14:09:50 +0000

    update

--- server/request_handler.lua
+++ server/request_handler.lua
@@ -10,8 +10,14 @@ local function alipay_trade_precreate(method, header, query, body)
     logger.debug('alipay_trade_precreate:query:%s,body:%s', query, body)
     return "OKxx"
 end
+
+local function wechat_auth_callback(method, header, query, body)
+    logger.debug('wechat_auth_callback:query:%s,body:%s', query, body)
+	return ""
+end
 local path_handler = {
     ["/"] = root,
+	["/wechat_auth_callback"] = wechat_auth_callback,
     ["/payment_notify/alipay_trade_precreate"] = alipay_trade_precreate, 
 }
 function request_handler.handle_request(path, method, header, query, body)
