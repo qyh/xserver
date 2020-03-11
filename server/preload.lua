c47d99e70d3a98ddd7b0b64785a370112b4ad2f2
qyh <qin6174@126.com>
2020-03-11 14:20:00 +0800

    update preload

--- server/preload.lua
+++ server/preload.lua
@@ -1,6 +1,6 @@
 -- This file will execute before every lua service start
 -- See config
-package.path = '../server/?.lua;../common/?.lua;../common/slaxml/?.lua;'
+package.path = '../server/?.lua;../common/?.lua;../common/slaxml/?.lua;../res/?.lua;'
 	.."../service/?.lua;"
 	..package.path..';'
 package.cpath = package.cpath..";./cservice/?.so;../luaclib/?.so;"
